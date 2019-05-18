03a225c54528036a375b30364fcbeee38db49308
vicente yah <vicente.yah.dzul@icloud.com>
2019-05-18 15:17:55 -0500

    template employee was created

---
+++ src/components/empleado.vue
@@ -0,0 +1,79 @@
+<template>
+    <nav>
+    <form @submit.prevent="consumirAPI">
+      <div class="form-group">
+        <label>Busqueda por Id del Empleado</label>
+        <div class="input-group">
+          <input v-model="id" class="form-control">
+          <button class="btn btn-dark" @click="consumirAPI">OK</button>
+        </div>
+      </div>
+    </form>
+
+    <table class="table table-striped">
+      <thead class="thead-dark">
+        <tr>
+          <th>ID</th>
+          <th>Nombre</th>
+          <th>Apellido Paterno</th>
+          <th>Apellido Materno</th>
+          <th>Telefono</th>
+          <th>Dirección</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="empleado in  datos_empleados" :key="empleado.id">
+          <td>{{ empleado.id }}</td>
+          <td>{{ empleado.nombre }}</td>
+          <td>{{ empleado.apaterno }}</td>
+          <td>{{ empleado.amaterno }}</td>
+          <td>{{ empleado.telefono }}</td>
+          <td>{{ empleado.direccion }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+</nav>
+  
+
+     
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "app",
+  data() {
+    return {
+      id : '',
+      datos_empleados: [],
+      url: "http://localhost:3000/datos_empleados/"
+    };
+  },
+  created() {
+  this.consumirAPI();
+  },
+  methods: {
+    consumirAPI () {
+      axios.get(this.url + this.id).then(response => {
+        console.log("passs")
+        this.datos_empleados = response.data;
+        console.log("yes");
+      }).catch(error => {
+        console.log(error)
+      });
+    }
+    
+  }
+};
+</script>
+  
+<style scoped>
+h2 {
+  color: #f44336;
+  font-size: 40 px;
+}
+div {
+  align-items: center;
+}
+</style>
